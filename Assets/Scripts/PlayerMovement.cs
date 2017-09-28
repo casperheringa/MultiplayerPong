69c262ee0516b7e4837d4c599830e2ae7e315590
Casper Heringa <5be8141b@opayq.com>
2017-09-28 11:26:07 +0200

    Update
    Full Game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour {
+
+	void Start(){
+		transform.Rotate (0, 0, 90);
+	}
+
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			Vector3 position = this.transform.position;
+			position.y += 0.3f;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			Vector3 position = this.transform.position;
+			position.y -= 0.3f;
+			this.transform.position = position;
+		}
+	}
+}
